7169036e97f57d832a095afaa77999209ceb94d3
mrismail91 <mohammad.ismail9190@gmail.com>
2020-04-14 19:13:13 +0300

    First commit

---
+++ gatsby-config.js
@@ -0,0 +1,54 @@
+module.exports = {
+  siteMetadata: {
+    title: `Unlimited Words`,
+    name: `Unlimted Words`,
+    siteUrl: `https://google.com`,
+    description: `This is my description that will be used in the meta tags and important for search results`,
+    hero: {
+      heading: `Welcome to Unlimted Words, a blog that brings you Science and Faith.`,
+      maxWidth: 652,
+    },
+    social: [
+      {
+        name: `twitter`,
+        url: `https://twitter.com`,
+      },
+      {
+        name: `instagram`,
+        url: `https://instagram.com`,
+      },
+    ],
+  },
+  plugins: [
+    {
+      resolve: "@narative/gatsby-theme-novela",
+      options: {
+        contentPosts: "content/posts",
+        contentAuthors: "content/authors",
+        basePath: "/",
+        authorsPage: true,
+        sources: {
+          local: true,
+          contentful: true,
+        },
+      },
+    },
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `Novela by Narative`,
+        short_name: `Novela`,
+        start_url: `/`,
+        background_color: `#fff`,
+        theme_color: `#fff`,
+        display: `standalone`,
+        icon: `src/assets/favicon.png`,
+      },
+    },
+    {
+      resolve: `gatsby-plugin-netlify-cms`,
+      options: {
+      },
+    },
+  ],
+};
